396eb0b320f1099bb411486f28a3bc351a207105
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-07-26 09:30:14 -0400

    first commit

---
+++ src/main/java/com/devyunes/ApiRest/DesafioDeCodigoApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.devyunes.ApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDeCodigoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDeCodigoApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devyunes/ApiRest/DesafioDeCodigoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devyunes.ApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDeCodigoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
