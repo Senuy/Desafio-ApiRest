b5d2523a7c95c5ac965a6c5c7379bd704b0116d1
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-08-04 14:36:03 -0400

    Bean Validation

--- src/main/java/com/devyunes/ApiRest/controller/ClientController.java
+++ src/main/java/com/devyunes/ApiRest/controller/ClientController.java
@@ -1,5 +1,7 @@
 package com.devyunes.ApiRest.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devyunes.ApiRest.dto.ClientDTO;
 import com.devyunes.ApiRest.services.ClientService;
@@ -31,13 +34,16 @@ public class ClientController {
 	}
 
 	@GetMapping
-	public Page<ClientDTO> findAll(Pageable pageable) {
+	public Page<ClientDTO> findAll(Pageable pageable) { 
 		return service.findAll(pageable);
 	}
 
 	@PostMapping
-	public ClientDTO insert(@Valid @RequestBody ClientDTO dto) {
-		return service.insert(dto);
+	public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 	@PutMapping(value = "/{id}")
